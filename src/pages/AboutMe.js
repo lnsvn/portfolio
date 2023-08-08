6e81846434237a832efdf7beb1acebdf0535cb5a
Lauren Sullivan <122634162+lnsvn@users.noreply.github.com>
2023-08-08 13:13:54 -0400

    adds a basic About Me page with an image of myself

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -1,9 +1,29 @@
 import React from "react";
+import me from "../images/me.jpg";
 
 function AboutMe() {
   return (
-    <main className="flex justify-center font-rem font-bold py-8">
-      <h1 className="text-3xl">About Me</h1>
+    <main className="flex flex-col items-center font-rem font-bold py-8">
+      <h1 className="text-3xl mb-7">About Me</h1>
+      <img
+        alt="lauren-sullivan"
+        className="w-52 h-52 rounded mb-7"
+        src={me}
+      ></img>
+      <p className="mb-7 text-center">Hello</p>
+      <p className="px-28 mb-7 text-center">
+        My name is Lauren. 
+        I am an aspiring Full-Stack Web Developer. 
+        I graduated from the UCONN Coding Bootcamp in July 2023. 
+        Take a look at my top projects displayed in the portfolio page. 
+        You will find links to the deployed applications and the corresponding GitHub repositories. 
+        Feel free to contact me with any questions or comments. 
+        You can also check out my GitHub, Indeed, or Twitter below.
+      </p>
+      <p className="px-28 text-center">
+        Outside of web development, my interests include 
+        photography, design, and reading.
+      </p>
     </main>
   );
 }
